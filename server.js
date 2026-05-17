5688b5a65f399993e53a607eb72b3a70b519833e
villagesnews123-wq <YOUR_GITHUB_EMAIL>
2026-05-17 23:12:23 +0530

    fixed youtube dl package

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
-const YTDlpWrap = require("yt-dlp-wrap").default;
+const dotenv = require("youtube-dl-exec");
+const youtubedl = ;
 
 dotenv.config();
 
@@ -9,8 +9,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-const ytDlpWrap = new YTDlpWrap("./bin/yt-dlp.exe");
 
 app.get("/", (req, res) => {
   res.json({
